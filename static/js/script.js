13aa24e9362815755cc115e31d2e4d76d87d0a57
NickCastPy <110970852+NickCastPy@users.noreply.github.com>
2022-12-29 22:33:03 +0200

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+const redirect__fn = () => {
+    window.scrollTo({
+      top:830,
+      left:0,
+      behavior: 'smooth'
+    })
+}
